806fc1de6d9409a310fced22b8dd02a7d8792a68
Sung Kim <sunggyun187@gmail.com>
2015-02-27 00:51:21 -0500

    working with bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,9 @@ class UsersController < ApplicationController
 	def index
 		
 		if user_signed_in?
-			# @tweets = twitter_client.user_timeline("Quant_LIfe").take(500)
-			# @id = current_user.id	
-			# Tweet.add_tweets_to_database(@tweets, @id)
+			@tweets = twitter_api.user_timeline("Quant_LIfe").take(500)
+			@id = current_user.id	
+			Tweet.add_tweets_to_database(@tweets, @id)
 
 			# TweetStream::Client.new.sample do |status|
  		# 		puts "#{status.text}"
