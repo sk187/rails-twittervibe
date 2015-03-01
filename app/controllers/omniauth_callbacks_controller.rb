bf87b2260ca80e2603f0c5408f44c20be33740ef
Sung Kim <sunggyun187@gmail.com>
2015-03-01 12:46:08 -0500

    fixed blank email unique constraint

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -13,7 +13,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     @user = @identity.user || current_user
     if @user.nil?
-      @user = User.create( email: @identity.email || "" )
+      @user = User.create( email: @identity.email || @identity.name )
       @identity.update_attribute( :user_id, @user.id )
     end
 
